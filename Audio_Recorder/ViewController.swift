c6fb84a2e2a3cd7f0e7f23b8811b528d44843336
max <max@ca.ibm.com>
2018-06-07 12:10:21 -0400

    Initial Commit

---
+++ Audio_Recorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Audio_Recorder
+//
+//  Created by Max Howarth on 2018-06-07.
+//  Copyright © 2018 Howarth Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
