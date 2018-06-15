6945b9f920e8a49db52dac4fa2fbb82bfb8b7394
max <max@ca.ibm.com>
2018-06-15 11:18:54 -0400

    Added UIConstants file so that I can store the gradient colours in one place. Modified gradient methods in viewDidLoad to use the colours from the constants file.

---
+++ Audio_Recorder/UIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  UIConstants.swift
+//  Audio_Recorder
+//
+//  Created by Max Howarth on 2018-06-15.
+//  Copyright © 2018 Howarth Technologies. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct UIConstants {
+    static let bottom: CGColor = UIColor(red: 15.0/225 , green: 32.0/255 ,blue: 39.0/255, alpha: 1.0).cgColor
+    static let middle:CGColor = UIColor(red: 42.0/225 , green: 75.0/255 ,blue: 85.0/255, alpha: 1.0).cgColor
+    static let top: CGColor = UIColor(red: 44.0/225 , green: 83.0/255 ,blue: 100.0/255, alpha: 1.0).cgColor
+}
--- Audio_Recorder/meetingViewController.swift
+++ Audio_Recorder/meetingViewController.swift
@@ -12,11 +12,7 @@ class meetingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var bottom: CGColor = UIColor(red: 15.0/225 , green: 32.0/255 ,blue: 39.0/255, alpha: 1.0).cgColor
-//        var bnottom2 = UIColor("2C5364)
-        var middle:CGColor = UIColor(red: 42.0/225 , green: 75.0/255 ,blue: 85.0/255, alpha: 1.0).cgColor
-        var top: CGColor = UIColor(red: 44.0/225 , green: 83.0/255 ,blue: 100.0/255, alpha: 1.0).cgColor
-        view.gradientLayer.colors = [ bottom, middle, top]
+        view.gradientLayer.colors = [ UIConstants.bottom, UIConstants.middle, UIConstants.top]
         view.gradientLayer.gradient = GradientPoint.bottomTop.draw()
         
 
